680b7ab26c12e0b82a27a7348c45ad4f1d03cd1a
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-04-07 10:20:14 +0300

    add Entity annotation

--- src/main/java/ru/bespalyy/model/Book.java
+++ src/main/java/ru/bespalyy/model/Book.java
@@ -1,24 +1,32 @@
 package ru.bespalyy.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
-import javax.validation.constraints.Past;
-import javax.validation.constraints.Pattern;
 import javax.validation.constraints.Size;
+import java.util.Objects;
 
+@Entity
+@Table(name = "book")
 public class Book {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "book_id")
     private Integer book_id;
 
+    @Column(name = "name")
     @NotEmpty(message = "Название книги не должно быть пустым")
     @Size(max = 100, message = "Максимальная длина не должна превышать 100 символов")
     private String name;
 
+    @Column(name = "author")
     @NotEmpty(message = "Поле автор не должно быть пустым")
     private String author;
 
-   // @Size(min = 0, message = "Год не может быть отрицательным")
-    //@Pattern(regexp = "\\d{1,4}", message = "Год не может содержать более 5 цифр")
+    @Column(name = "year")
     private int year;
 
+@Column(name = "person_id")
     private Integer person_id;
 
     public Book() {
@@ -70,5 +78,29 @@ public class Book {
 
     public void setPerson_id(Integer person_id) {
         this.person_id = person_id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Book book = (Book) o;
+        return year == book.year && Objects.equals(book_id, book.book_id) && Objects.equals(name, book.name) && Objects.equals(author, book.author);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(book_id, name, author, year);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "book_id=" + book_id +
+                ", name='" + name + '\'' +
+                ", author='" + author + '\'' +
+                ", year=" + year +
+                ", person_id=" + person_id +
+                '}';
     }
 }
