a938ff18900028a153ee21fde2416cca4fb820eb
Sergey Bespalyy <bespaliy.sergei@yandex.ru>
2023-04-07 10:20:21 +0300

    add Entity annotation

--- src/main/java/ru/bespalyy/model/Person.java
+++ src/main/java/ru/bespalyy/model/Person.java
@@ -1,13 +1,23 @@
 package ru.bespalyy.model;
 
+import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.util.Objects;
 
+@Entity
+@Table(name = "Person")
 public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "person_id")
     private Integer person_id;
 
+    @Column(name = "family")
     @NotEmpty(message = "Фамилия не может быть пустой")
     private String family;
 
+    @Column(name = "yearbirthday")
     private Integer yearBirthday;
 
     public Person() {
@@ -43,4 +53,25 @@ public class Person {
         this.person_id = person_id;
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Person person = (Person) o;
+        return Objects.equals(person_id, person.person_id) && Objects.equals(family, person.family) && Objects.equals(yearBirthday, person.yearBirthday);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(person_id, family, yearBirthday);
+    }
+
+    @Override
+    public String toString() {
+        return "Person{" +
+                "person_id=" + person_id +
+                ", family='" + family + '\'' +
+                ", yearBirthday=" + yearBirthday +
+                '}';
+    }
 }
